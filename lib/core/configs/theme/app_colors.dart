3788c0d09a57287504e417bb7775637a91b50bb7
makh <kmohammadali66@yahoo.com>
2024-11-22 12:51:16 +0330

    refactored AppColors name & implemented theme

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class AppColor {
+class AppColors {
   static const primary = Color(0xffE21221);
   static const background = Color(0xff121A20);
   static const secondBackground = Color(0xff35383F);
