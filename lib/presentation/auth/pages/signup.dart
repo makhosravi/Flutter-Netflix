092e4825fcd22e4b5849812ad18439d11e232613
makh <kmohammadali66@yahoo.com>
2024-12-05 08:59:49 +0330

    implemented sign up page

--- lib/presentation/auth/pages/signup.dart
+++ lib/presentation/auth/pages/signup.dart
@@ -1,10 +1,82 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_movie/common/helper/navigation/app_navigation.dart';
+import 'package:flutter_movie/core/configs/theme/app_colors.dart';
+import 'package:flutter_movie/presentation/auth/pages/signin.dart';
+import 'package:reactive_button/reactive_button.dart';
 
 class SignupPage extends StatelessWidget {
   const SignupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        minimum: const EdgeInsets.only(top: 100.0, right: 16.0, left: 16.0,),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _singupText(),
+          const SizedBox(height: 30.0,),
+          _emailField(),
+          const SizedBox(height: 20.0,),
+          _passwordField(),
+          const SizedBox(height: 60.0,),
+          _signupButton(),
+          const SizedBox(height: 20.0,),
+          _singinText(context),
+        ],
+      )),
+    );
+  }
+
+  Widget _singupText() {
+    return const Text('Sing up', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0,),);
+  }
+
+  Widget _emailField() {
+    return const TextField(
+      decoration: InputDecoration(
+        hintText: 'Email',
+      ),
+    );
+  }
+
+  Widget _passwordField() {
+    return const TextField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+      ),
+    );
+  }
+
+  Widget _signupButton() {
+    return ReactiveButton(
+      title: 'Sing up',
+      activeColor: AppColors.primary,
+    onPressed: () async {}, 
+    onSuccess: (){}, 
+    onFailure: (error){},
+    );
+  }
+
+  Widget _singinText(BuildContext context) {
+    return Text.rich(
+      TextSpan(
+        children: [
+          const TextSpan(
+            text: "Already have an account?",
+          ),
+          TextSpan(
+            text: ' Sign In ',
+            style: const TextStyle(color: Colors.blue),
+            recognizer: TapGestureRecognizer()..onTap = (){
+              AppNavigator.push(context, const SigninPage(),);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
