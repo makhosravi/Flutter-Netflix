07cd41dcf61b262e9901fdf24793ea0e0f29bef2
makh <kmohammadali66@yahoo.com>
2025-01-28 18:09:38 +0330

    added shared preferences in presentation

--- lib/presentation/splash/bloc/splash_cubit.dart
+++ lib/presentation/splash/bloc/splash_cubit.dart
@@ -1,11 +1,18 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_movie/domain/auth/usecases/is_logged_in.dart';
 import 'package:flutter_movie/presentation/splash/bloc/splash_state.dart';
+import 'package:flutter_movie/service_locator.dart';
 
 class SplashCubit extends Cubit<SplashState> {
   SplashCubit() : super(Displaysplash());
 
   void appStarted() async {
     await Future.delayed(const Duration(seconds: 2));
+    var isLoggedIn = await sl<IsLoggedInUsecase>().call();
+    if (isLoggedIn){
+      emit(Authenticated());
+    }else{
     emit(Unauthenticated());
+    }
   }
 }
