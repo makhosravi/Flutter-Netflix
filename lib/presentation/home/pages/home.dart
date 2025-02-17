cb2d6773e35dd1ee1da6098eeb3e3f6605dbbef2
makh <kmohammadali66@yahoo.com>
2025-02-17 20:14:38 +0330

    refactor: add trending text widget & reposioned widgets

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movie/common/widgets/appbar/app_bar.dart';
 import 'package:flutter_movie/core/configs/assets/app_vectors.dart';
-import 'package:flutter_movie/presentation/home/widgets/trending.dart';
+import 'package:flutter_movie/presentation/home/widgets/trending_movies.dart';
+import 'package:flutter_movie/presentation/home/widgets/trending_text.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
@@ -12,20 +13,20 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: BasicAppbar(
         hideBack: true,
-        leading: Padding(
-          padding: const EdgeInsets.only(
-            left: 10.0,
-          ),
-          child: SvgPicture.asset(
-            AppVectors.logo,
-          ),
+        title: SvgPicture.asset(
+          AppVectors.logo,
         ),
       ),
-      body: const Column(
-        children: [
-          TrendingMovies(),
-        ],
+      body: const SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TrendingText(),
+            TrendingMovies(),
+          ],
+        ),
       ),
     );
   }
 }
+
