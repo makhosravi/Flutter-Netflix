f8563eb3f242bef5fe7d98317ddf8d5675e41280
makh <kmohammadali66@yahoo.com>
2025-04-14 19:09:48 +0330

    added watch movie trailer details

--- lib/presentation/watch/pages/watch.dart
+++ lib/presentation/watch/pages/watch.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movie/common/widgets/appbar/app_bar.dart';
 import 'package:flutter_movie/domain/movie/enteties/movie_entity.dart';
+import 'package:flutter_movie/presentation/watch/widgets/video_overview.dart';
 import 'package:flutter_movie/presentation/watch/widgets/video_player.dart';
+import 'package:flutter_movie/presentation/watch/widgets/video_release_date.dart';
+import 'package:flutter_movie/presentation/watch/widgets/video_title.dart';
+import 'package:flutter_movie/presentation/watch/widgets/video_vote_average.dart';
 
 class MovieWatchPage extends StatelessWidget {
   const MovieWatchPage({
@@ -21,8 +25,39 @@ class MovieWatchPage extends StatelessWidget {
         padding: const EdgeInsets.symmetric(
           horizontal: 16.0,
         ),
-        child: VideoPlayer(
-          id: movieEntity.id,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            VideoPlayer(
+              id: movieEntity.id,
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            VideoTitle(
+              title: movieEntity.title,
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                VideoReleaseDate(
+                  releaseDate: movieEntity.releaseDate!,
+                ),
+                VideoVoteAverage(
+                  voteAverage: movieEntity.voteAverage,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            VideoOverview(
+              overview: movieEntity.overview,
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/presentation/watch/widgets/video_overview.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class VideoOverview extends StatelessWidget {
+  final String overview;
+  const VideoOverview({
+    required this.overview,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Overview',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          overview,
+          style: Theme.of(context).primaryTextTheme.bodyMedium,
+        ),
+      ],
+    );
+  }
+}
