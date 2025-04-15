21cc4b09ebbbe575623b1c958210b91dadf46c87
makh <kmohammadali66@yahoo.com>
2025-04-16 00:35:08 +0330

    bugfix error in trending movies & deleted unnecessarry files

--- lib/presentation/home/widgets/trending_movies.dart
+++ lib/presentation/home/widgets/trending_movies.dart
@@ -1,9 +1,12 @@
 import 'package:fan_carousel_image_slider/fan_carousel_image_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_movie/common/bloc/generic_data_cubit.dart';
+import 'package:flutter_movie/common/bloc/generic_data_state.dart';
 import 'package:flutter_movie/core/configs/assets/app_images.dart';
-import 'package:flutter_movie/presentation/home/bloc/trendings_cubit.dart';
-import 'package:flutter_movie/presentation/home/bloc/trendings_state.dart';
+import 'package:flutter_movie/domain/movie/enteties/movie_entity.dart';
+import 'package:flutter_movie/domain/movie/usecases/get_trending_movies.dart';
+import 'package:flutter_movie/service_locator.dart';
 
 class TrendingMovies extends StatelessWidget {
   const TrendingMovies({super.key});
@@ -11,18 +14,26 @@ class TrendingMovies extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => TrendingCubit()..getTrendingMovies(),
-      child: BlocBuilder<TrendingCubit, TrendingState>(
+      create: (context) => GenericDataCubit()
+        ..getData<List<MovieEntity>>(
+          sl<GetTrendingMoviesUsecase>(),
+        ),
+      child: BlocBuilder<GenericDataCubit, GenericDataState>(
         builder: (context, state) {
-          if (state is TrendingMoviesLoading) {
-            return const Center(child: CircularProgressIndicator());
+          if (state is DataLoadind) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           }
 
-          if (state is TrendingMoviesLoaded) {
+          if (state is DataLoaded) {
             return FanCarouselImageSlider.sliderType1(
-              imagesLink: state.movies
-                  .map((item) =>
-                      AppImages.movieImageBasePath + item.posterPath.toString())
+              imagesLink: state.data
+                  .map<String>(
+                    (item) =>
+                        AppImages.movieImageBasePath +
+                        item.posterPath!.toString(),
+                  )
                   .toList(),
               isAssets: false,
               autoPlay: false,
@@ -31,7 +42,7 @@ class TrendingMovies extends StatelessWidget {
             );
           }
 
-          if (state is FailureLoadTrendingMovies) {
+          if (state is FailureData) {
             return Text(state.errorMessage);
           }
 
